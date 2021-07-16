d2ed8979393af8521bee98654950174ce991d472
D3M80L <marcin.dembowski@gmail.com>
2021-07-17 01:30:22 +0200

    feat: white and color modes

--- README.md
+++ README.md
@@ -7,17 +7,29 @@ It allows to integrate with MiPow Playbulbs - bluetooth, battery controlled LED 
  - BTL300
  - BTL305ES
 
+## Changelog
+### 1.0.0
+- better support of light control - supporting separate white and color modes
+- updated effects - all in lowercase
+- introduced random and white effect
+- battery level scan is performed on every 10th update when the candle is turned off
+- status update is locked when three failed connections occure in a row
+- on startup (first run) the effect is fetched from the device
+- fetching device attributes like model, software version or manufacturer
+
 ## Installation
 
 ### HACS
  - Ensure HACS is installed
  - Add Custom Repository https://github.com/D3M80L/hassio-mipow
  - Install the integration
+ - Configure your candles in configuration.yaml
  - Restart Home Assistant
 
 ### Manual installation
  - Download the latest release
  - Unpack the release and copy *mipow* folder from the *custom_components* folder in this repository, to the folder *custom_components* in your Home Assistant installation
+ - Configure your candles in configuration.yaml
  - Restart Home Assistant
 
 ## Configuration
@@ -28,12 +40,15 @@ You need to know the MAC address of your device and use it in the configuration.
 ```yaml
 light:
   - platform: mipow
+    scan_interval: 90
     devices:
       "AA:BB:CC:DD:EE:FF":
         name: Name Your Device
       "FE:ED:AF:AC:E0:00":
         name: Name Your Second Device
 ```
+### scan_interval
+By default is set to 30 seconds in homeassistant, set it to higher value if you control the candle only from HA.
 
 ## Supported features
 After restart, when the integration successfully connects to your device, you can control your candle directly from Lovelace card or by sending a service command:
@@ -45,7 +60,53 @@ rgb_color:
   - 255
   - 0
 brightness: 0..255
-effect: Flash | Candle | Fade | Jump RGB | Fade RGB
+effect: light | flash | pulse | rainbow | colorloop | candle | random | white
 white_value: 0..255
 flash: short | long
+transition: 0..255
 ```
+
+### Color mode
+In color mode you can control both white value and color separately.
+When color is selected, the brigthness of the color is only changed, in this case the white value is controlled separately.
+When no color is selected, then brightness represents white value.
+![Color palette](doc/color_palette.png "Example color palette")
+
+### White mode
+In this mode only white value is changed - any colors are removed.
+![White mode](doc/white_mode.png "Example white mode")
+
+### Effects
+The device supports effects built into the device and available only in HA.
+![Effect list](doc/effects.png "Supported effects")
+
+#### Build in candle effects
+Mipow candles come with a predefined list of effects that are represented in HA by:
+- light - no effects applied at all
+- flash - flash the candle
+- pulse
+- rainbow - change the light color
+- colorloop - is a combination of rainbow and pulse
+- candle - blink randombly the light
+
+The effect speed can be controller by transition parameter.
+
+#### Random
+The 'random' effect is available only in HA.
+On every HA update (see scan_interval) or manual change a random RGB color is set.
+The brightness of the color is preserved for any new random colors and white value is not changed - you can adjust white value separately.
+The randomization can be set on top of the other effects and is not supported only when colorloop or rainbow effect is enabled.
+To disable random colors, set another effect. The effect is not preserved on HA restart.
+To enable random colors for candle effect - first enable candle effect and then set the random effect.
+
+#### White
+This effect also works on top of other build-in effects (excluding rainbow and colorloop). Like with 'random' effect, you have to select first the build-in effect (like light) and then select 'white'. This effect spans brightness and white value together - any modification of those parameters changes also the  other. Note that selecting this effect may quicker drain your battery.
+
+### Transition
+Transition might be changed in future releases.
+The value range between 0 and 255 can be used to control the speed of some built-in effects like pulse, flash, rainbow or colorloop
+
+### Attributes
+Also few device attributes are fetched from the device. 
+Except battery level, rest is fetched only once at initialization.
+![Attribute list](doc/attributes.png "Example attributes")
--- custom_components/mipow/light.py
+++ custom_components/mipow/light.py
@@ -1,193 +1,394 @@
-"""Platform for light integration."""
 from .mipow import MipowDevice
+from typing import Final
 
 import logging
-
+import random
 import voluptuous as vol
 
 import homeassistant.helpers.config_validation as cv
-from homeassistant.helpers.entity import async_generate_entity_id
-# Import the device class from the component that you want to support
+
 from homeassistant.components.light import (
-	ATTR_BRIGHTNESS,
-	ATTR_HS_COLOR,
-	ATTR_EFFECT,
-	ATTR_WHITE_VALUE,
-	ATTR_FLASH,
-	FLASH_SHORT,
-	FLASH_LONG,
-	PLATFORM_SCHEMA,
-	SUPPORT_BRIGHTNESS,
-	SUPPORT_COLOR,
-	SUPPORT_EFFECT,
-	SUPPORT_WHITE_VALUE,
-	LightEntity)
-from homeassistant.const import CONF_DEVICES, CONF_NAME, ATTR_BATTERY_LEVEL
+    ATTR_BRIGHTNESS,
+    ATTR_RGBW_COLOR,
+    ATTR_EFFECT,
+    ATTR_WHITE,
+    ATTR_FLASH,
+    ATTR_TRANSITION,
+    FLASH_SHORT,
+    FLASH_LONG,
+    PLATFORM_SCHEMA,
+    SUPPORT_EFFECT,
+    SUPPORT_FLASH,
+    SUPPORT_TRANSITION,
+    COLOR_MODE_RGBW,
+    COLOR_MODE_WHITE,
+    EFFECT_COLORLOOP,
+    EFFECT_RANDOM,
+    EFFECT_WHITE,
+    LightEntity)
+from homeassistant.const import (
+    CONF_DEVICES,
+    CONF_NAME,
+    ATTR_BATTERY_LEVEL,
+    ATTR_MODEL,
+    ATTR_MANUFACTURER,
+    ATTR_SW_VERSION)
+
 import homeassistant.util.color as color_util
 
 _LOGGER = logging.getLogger(__name__)
 
-SUPPORT_MIPOW_LED = SUPPORT_BRIGHTNESS | SUPPORT_COLOR | SUPPORT_EFFECT | SUPPORT_WHITE_VALUE
+SUPPORT_MIPOW_LED = SUPPORT_EFFECT | SUPPORT_FLASH | SUPPORT_TRANSITION
+BATTERY_SCAN_INTERVAL:Final="battery_scan_every"
 
-DEVICE_SCHEMA = vol.Schema({vol.Optional(CONF_NAME): cv.string})
+MIPOW_EFFECT_PULSE:Final = "pulse"
+MIPOW_EFFECT_FLASH:Final = "flash"
+MIPOW_EFFECT_CANDLE:Final = "candle"
+MIPOW_EFFECT_LIGHT:Final = "light"
+MIPOW_EFFECT_RAINBOW:Final = "rainbow"
+
+MIPOW_EFFECT_LIGHT_CODE:Final = 255
+MAX_FAILED_STATUS_UPDATES_IN_ROW:Final = 3
+
+DEVICE_SCHEMA = vol.Schema(
+    {
+        vol.Optional(CONF_NAME): cv.string,
+        vol.Optional(BATTERY_SCAN_INTERVAL, default=10): cv.positive_int
+    })
 
 PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend(
-	{vol.Optional(CONF_DEVICES, default={}): {cv.string: DEVICE_SCHEMA}}
+    {
+        vol.Optional(CONF_DEVICES, default={}): 
+        {
+            cv.string: DEVICE_SCHEMA
+        }
+    }
 )
 
 CandleEffectsMap = {
-	"Flash" : 0,
-	"Fade" : 1,
-	"Jump RGB" : 2,
-	"Fade RGB" : 3,
-	"Candle" : 4,
-	"None" : 255
+    "Flash" : 0,
+    MIPOW_EFFECT_FLASH: 0,
+    "Fade" : 1,
+    MIPOW_EFFECT_PULSE: 1,
+    "Jump RGB" : 2,
+    MIPOW_EFFECT_RAINBOW: 2,
+    "Fade RGB" : 3,
+    EFFECT_COLORLOOP: 3,
+    "Candle" : 4,
+    MIPOW_EFFECT_CANDLE: 4,
+    "None" : 255,
+    MIPOW_EFFECT_LIGHT: MIPOW_EFFECT_LIGHT_CODE
+}
+
+MipowEffectMap = {
+    0: MIPOW_EFFECT_FLASH,
+    1: MIPOW_EFFECT_PULSE,
+    2: MIPOW_EFFECT_RAINBOW,
+    3: EFFECT_COLORLOOP,
+    4: MIPOW_EFFECT_CANDLE,
+    MIPOW_EFFECT_LIGHT_CODE: MIPOW_EFFECT_LIGHT
 }
 
 def setup_platform(hass, config, add_entities, discovery_info=None):
-	# Add devices
-	lights = []
-	for address, device_config in config[CONF_DEVICES].items():
-		name = device_config[CONF_NAME];
-		light = MipowCandle(MipowDevice(address), name)
-		lights.append(light)
+    # Add devices
+    lights = []
+    for address, device_config in config[CONF_DEVICES].items():
+        name = device_config[CONF_NAME]
+        battery_interval = device_config[BATTERY_SCAN_INTERVAL]
+        light = MipowCandle(MipowDevice(address), name, battery_interval)
+        lights.append(light)
 
-	add_entities(light for light in lights)
+    add_entities(light for light in lights)
 
 class MipowCandle(LightEntity):
-	def __init__(self, light, name):
-		self._light = light
-		self._name = name
-		self._state = False
-		self._brightness = 128
-		self._hs_color = (1, 0)
-		self._white = 0
-		self._effect = None
-		self._unique_id = f"{self.__class__}.{light.mac}"
-		self._is_connected = False
-		self._battery_level = None
-		self._failed_updates_count = 0
+    def __init__(self, light, name:str, battery_scan_every:int):
+        self._attr_brightness:int = 128
+        self._attr_rgbw_color = (0, 0, 0, 128)
+        self._attr_effect:str = MIPOW_EFFECT_LIGHT
+        self._version = None
+        self._model = None
+        self._manufacturer = None
+        self._is_random:bool = False
+        self._is_white:bool = False
+        self._first_status_checked:bool = False
 
-	@property
-	def name(self):
-		return self._name
+        self._transition:int = 0x14
+        self._light = light
+        self._name:str = name
+        self._state:bool = False
+        self._unique_id:str = f"{self.__class__}.{light.mac}"
+        self._is_connected:bool = False
+        self._battery_level = None
+        self._failed_updates_count:int = 0
+        self._battery_level_update_count:int = 0
+        self._battery_scan_every:int = battery_scan_every
+        self._attr_supported_color_modes = [COLOR_MODE_WHITE, COLOR_MODE_RGBW]
+        self._attr_effect_list = [
+            MIPOW_EFFECT_LIGHT, 
+            MIPOW_EFFECT_CANDLE, 
+            MIPOW_EFFECT_PULSE, 
+            MIPOW_EFFECT_FLASH,
+            EFFECT_COLORLOOP,
+            MIPOW_EFFECT_RAINBOW,
+            EFFECT_RANDOM,
+            EFFECT_WHITE]
 
-	@property
-	def brightness(self):
-		return self._brightness
+    @property
+    def brightness(self) -> int:
+        return self._attr_brightness
 
-	@property
-	def hs_color(self):
-		return self._hs_color
+    @property
+    def rgbw_color(self):
+        return self._attr_rgbw_color
 
-	@property
-	def supported_features(self):
-		return SUPPORT_MIPOW_LED
+    @property
+    def name(self) -> str:
+        return self._name
 
-	@property
-	def is_on(self):
-		return self._state
+    @property
+    def supported_features(self):
+        return SUPPORT_MIPOW_LED
 
-	@property
-	def effect_list(self):
-		return [x for x in CandleEffectsMap.keys()]
+    @property
+    def is_on(self) -> bool:
+        return self._state
 
-	@property
-	def effect(self):
-		return self._effect
+    @property
+    def effect_list(self):
+        return self._attr_effect_list
 
-	@property
-	def white_value(self):
-		return self._white
+    @property
+    def effect(self) -> str:
+        return self._attr_effect
 
-	@property
-	def unique_id(self):
-		return self._unique_id
+    @property
+    def white_value(self) -> int:
+        return self.rgbw_color[3]
 
-	@property
-	def device_state_attributes(self):
-		return {
-			(ATTR_BATTERY_LEVEL, self._battery_level)
-		}
+    @property
+    def unique_id(self):
+        return self._unique_id
 
-	def turn_on(self, **kwargs):
-		brightness = self._brightness
-		hsColor = self._hs_color
-		white = self._white
-		effect = self._effect
-		effect_delay = 0x14
+    @property
+    def is_random(self) -> bool:
+        return self._is_random
 
-		if ATTR_BRIGHTNESS in kwargs:
-			brightness = kwargs.get(ATTR_BRIGHTNESS, 255)
+    @property
+    def device_state_attributes(self):
+        return {
+            (ATTR_BATTERY_LEVEL, self._battery_level),
+            (ATTR_MODEL, self._model),
+            (ATTR_SW_VERSION, self._version),
+            (ATTR_MANUFACTURER, self._manufacturer)
+        }
 
-		if ATTR_HS_COLOR in kwargs:
-			hsColor = kwargs.get(ATTR_HS_COLOR)
+    def turn_on(self, **kwargs):
+        brigtnessWasSet:bool = ATTR_BRIGHTNESS in kwargs
+        brightness:int = self.brightness
+        isWhite:int = self._is_white
+        rgbw_color = self.rgbw_color
 
-		if ATTR_WHITE_VALUE in kwargs:
-			white = kwargs.get(ATTR_WHITE_VALUE)
+        effect:str = self.effect
+        transition:int = self._transition
 
-		if ATTR_EFFECT in kwargs:
-			effect = kwargs.get(ATTR_EFFECT)
-			if (not effect in CandleEffectsMap):
-				effect = None
+        if ATTR_EFFECT in kwargs:
+            effect = kwargs.get(ATTR_EFFECT)
+            self._is_random = False
+            isWhite = False
+            if (effect == EFFECT_RANDOM):
+                self._is_random = True
+                effect = self.effect
+            elif (effect == EFFECT_WHITE):
+                isWhite = True
+                effect = self.effect
+            else:
+                if (not effect in CandleEffectsMap):
+                    effect = MIPOW_EFFECT_LIGHT
 
-		if ATTR_FLASH in kwargs:
-			effect = 'Flash'
-			flash = kwargs.get(ATTR_FLASH)
-			if flash == FLASH_LONG:
-				effect_delay = 0x30
-			elif flash == FLASH_SHORT:
-				effect_delay = 0x10
+        if ATTR_FLASH in kwargs:
+            effect = MIPOW_EFFECT_FLASH
+            flash = kwargs.get(ATTR_FLASH)
+            if flash == FLASH_LONG:
+                transition = 0x30
+            elif flash == FLASH_SHORT:
+                transition = 0x10
 
-		rgbColor = color_util.color_hsv_to_RGB(
-		  hsColor[0], hsColor[1], brightness / 255 * 100
-		)
+        if ATTR_TRANSITION in kwargs:
+            transition = int(kwargs.get(ATTR_TRANSITION, 0x14))
+            if (transition > 255):
+                transition = 255
 
-		effectId = None
-		if (not effect is None):
-			effectId = CandleEffectsMap[effect]
-			if (effectId == 255):
-				effectId = None
+        if ATTR_RGBW_COLOR in kwargs:
+            rgbw_color = kwargs.get(ATTR_RGBW_COLOR)
 
-		self._connect()
-		if (effectId is None):
-			self._light.set_rgbw(rgbColor[0], rgbColor[1], rgbColor[2], white)
-		else:
-			self._light.set_effect(rgbColor[0], rgbColor[1], rgbColor[2], white, effectId, delay=effect_delay)
-			
-		self._hs_color = hsColor
-		self._brightness = brightness
-		self._white = white
-		self._effect = effect
-		self._state = True
+        if brigtnessWasSet:
+            brightness = kwargs.get(ATTR_BRIGHTNESS, 255)
 
-	def turn_off(self, **kwargs):
-		self._connect()
-		self._light.set_rgbw(0, 0, 0, 0)
-		self._state = False
+        if ATTR_WHITE in kwargs:
+            brightness = kwargs.get(ATTR_WHITE)
+            rgbw_color = (0,0,0, brightness)
+            isWhite = False
+            brigtnessWasSet = True
 
-	def _connect(self):
-		if (not self._is_connected):
-			self._light.connect()
-			self._is_connected = True
-			self._failed_updates_count = 0
+        if (isWhite):
+            if (not brigtnessWasSet):
+                if (self._is_only_white(rgbw_color)):
+                    brightness = rgbw_color[3]
+                elif (rgbw_color[3] != self.rgbw_color[3]):
+                    brightness = rgbw_color[3]
+                else:
+                    hsv = color_util.color_RGB_to_hsv(rgbw_color[0], rgbw_color[1], rgbw_color[2])
+                    brightness = int(hsv[2]/100*255)
 
-	def update(self):
-		if (self._failed_updates_count > 10):
-			return
+            rgbw_color = (brightness,brightness,brightness,brightness)
+        elif (brigtnessWasSet):
+            if (self._is_only_white(rgbw_color)):
+                rgbw_color = (0, 0, 0, brightness)
+            else:
+                hsv = color_util.color_RGB_to_hsv(rgbw_color[0], rgbw_color[1], rgbw_color[2])
+                rgb_color = color_util.color_hsv_to_RGB(hsv[0], hsv[1], int(brightness/255*100))
+                rgbw_color = (rgb_color[0], rgb_color[1], rgb_color[2], rgbw_color[3])
 
-		try:
-			self._connect()
-			result = self._light.fetch_rgbw()
-			self._state = result[0] != 0 or result[1] != 0 or result[2] != 0 or result[3] != 0
-			if (self._state):
-				self._white = result[0]
-				hsv = color_util.color_RGB_to_hsv(result[1], result[2], result[3])
-				self._hs_color = hsv[:2]
-				self._brightness = (hsv[2] / 100) * 255
+        if (self._is_rgbw_zero(rgbw_color)):
+            self.turn_off()
+            return
 
-			self._battery_level = self._light.fetch_battery_level()		
-		except:
-			self._is_connected = False
-			self._battery_level = None
-			self._failed_updates_count += 1
+        self._connect()
+        
+        effectId:int = self._get_effect_id(effect)
+        self._set_light(rgbw_color, effectId, transition)
+
+        self._state = True
+        self._battery_level_update_count = 0
+        self._attr_effect = effect
+        self._transition = transition
+        self._is_white = isWhite
+        self._attr_effect = effect
+
+        self._set_attributes(rgbw_color)
+
+    def turn_off(self, **kwargs):
+        self._connect()
+        self._light.set_rgbw(0, 0, 0, 0)
+        self._state = False
+        self._battery_level_update_count = 0
+
+    def update(self):
+        if (self._failed_updates_count > MAX_FAILED_STATUS_UPDATES_IN_ROW):
+            return
+
+        try:
+            self._connect()
+
+            firstStatusChecked:bool = self._first_status_check()
+
+            result = self._light.fetch_rgbw()
+            self._state = not self._is_rgbw_zero(result)
+
+            if (self._state):
+                if (self.is_random):
+                    random_rgbw = self._set_random_colors(result[1], result[2], result[3], result[0], self.effect)
+                    result = (random_rgbw[3], random_rgbw[0], random_rgbw[1], random_rgbw[2])
+
+                self._set_attributes((result[1], result[2], result[3], result[0]))
+
+            if (self._state or self._battery_level_update_count % self._battery_scan_every == 0):
+                self._battery_level = self._light.fetch_battery_level()
+                self._battery_level_update_count = 0
+
+            self._battery_level_update_count += 1
+
+            if (not firstStatusChecked):
+                if (self._state):
+                    effectId = self._get_effect_id(self.effect)
+                    self._set_light(self.rgbw_color, effectId, self._transition)
+                    self._is_white = self._is_white_effect(self.rgbw_color)
+                else:
+                    self.turn_off()
+                
+        except:
+            self._is_connected = False
+            self._battery_level = None
+            self._failed_updates_count += 1
+            self._battery_level_update_count = 0
+            self._first_status_checked = False
+
+            if (self._failed_updates_count > MAX_FAILED_STATUS_UPDATES_IN_ROW):
+                _LOGGER.warning('Skipping status update checks for %s. Check battery status and toggle the candle in home assistant.', self._name)
+
+            raise
+    
+    def _connect(self):
+        try:
+            if (not self._is_connected):
+                self._light.connect()
+                self._is_connected = True
+                self._failed_updates_count = 0
+                if (self._version is None):
+                    self._version = self._light.fetch_hardware()
+
+                if (self._model is None):
+                    self._model = self._light.fetch_model()
+
+                if (self._manufacturer is None):
+                    self._manufacturer = self._light.fetch_manufacturer()
+        except:
+            self._is_connected = False
+            self._first_status_checked = False
+            raise
+
+    def _is_rgbw_zero(self, rgbw) -> bool:
+        return rgbw[0] == 0 and rgbw[1] == 0 and rgbw[2] == 0 and rgbw[3] == 0
+
+    def _is_only_white(self, rgbw) -> bool:
+        return rgbw[0] == 0 and rgbw[1] == 0 and rgbw[2] == 0
+
+    def _is_white_effect(self, rgbw) -> bool:
+        return rgbw[0] == rgbw[1] == rgbw[2] == rgbw[3]
+
+    def _set_attributes(self, rgbw):
+        hsv = color_util.color_RGB_to_hsv(rgbw[0], rgbw[1], rgbw[2])
+        self._attr_rgbw_color = rgbw
+        self._attr_hs_color = hsv[:2]
+        self._attr_brightness = (hsv[2] / 100) * 255
+        if (self._is_only_white(rgbw)):
+            self._attr_brightness = rgbw[3]
+
+    def _get_effect_id(self, effectName) -> int:
+        if (effectName is None):
+            return MIPOW_EFFECT_LIGHT_CODE
+
+        return CandleEffectsMap[effectName]
+
+    def _set_light(self, rgbw_color, effectId:int, transition:int):
+        self._light.set_rgbw(rgbw_color[0], rgbw_color[1], rgbw_color[2], rgbw_color[3])
+        if (effectId != MIPOW_EFFECT_LIGHT_CODE):
+            self._light.set_effect(rgbw_color[0], rgbw_color[1], rgbw_color[2], rgbw_color[3], effectId, delay=transition)
+
+    def _set_random_colors(self, r:int, g:int, b:int, w:int, effect:str):
+        effectId:int = self._get_effect_id(effect)
+        hsv = color_util.color_RGB_to_hsv(r, g, b)
+        
+        minSaturation:int = 75 
+        if (effect == MIPOW_EFFECT_PULSE):
+            minSaturation = 100 # Seems that PULSE effect only accepts full saturation
+
+        hsv = (random.randint(0, 359), random.randint(minSaturation, 100), hsv[2])
+        rgb = color_util.color_hsv_to_RGB(hsv[0], hsv[1], hsv[2])
+        rgbw = (rgb[0], rgb[1], rgb[2], w)
+        self._set_light(rgbw, effectId, self._transition)
+        return rgbw
+
+    def _first_status_check(self):
+        if (self._first_status_checked):
+            return True
+
+        self._connect()
+        self._first_status_checked = True
+        
+        resultEffect = self._light.fetch_effect()
+        self._transition = resultEffect[6]
+        effectId:int = resultEffect[4]
+        if (effectId in MipowEffectMap):
+            self._attr_effect = MipowEffectMap[effectId]
+        return False
